8247964315a230a42a09dfca11cb9fbf71d5a4e3
Omar Jallouq <omarjallouq@gmail.com>
2023-11-07 13:14:32 +0100

    Added functionality to cyphers - Vigenere and OJ

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function ojCypher(message){
+function ojCipher(message){
       // Convert the string into an array of characters
   let characters = message.split("");
   
@@ -14,9 +14,87 @@ function ojCypher(message){
   return characters.join("");
 }
 
+function vigenereEncrypt(plaintext, keyword) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let key = keyword.toLowerCase();
+    let result = '';
+  
+    for (let i = 0, j = 0; i < plaintext.length; i++) {
+      let char = plaintext[i];
+      if (char.match(/[a-zA-Z]/)) {
+        const isUpperCase = char === char.toUpperCase();
+        char = char.toLowerCase();
+        const charIndex = alphabet.indexOf(char);
+        const keyChar = key[j % key.length];
+        const keyIndex = alphabet.indexOf(keyChar);
+        let shiftedIndex = (charIndex + keyIndex) % 26;
+        let shiftedChar = alphabet[shiftedIndex];
+        if (isUpperCase) {
+          shiftedChar = shiftedChar.toUpperCase();
+        }
+        result += shiftedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+  
+    return result;
+}
+  
+function vigenereDecrypt(ciphertext, keyword) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let key = keyword.toLowerCase();
+    let result = '';
+  
+    for (let i = 0, j = 0; i < ciphertext.length; i++) {
+      let char = ciphertext[i];
+      if (char.match(/[a-zA-Z]/)) {
+        const isUpperCase = char === char.toUpperCase();
+        char = char.toLowerCase();
+        const charIndex = alphabet.indexOf(char);
+        const keyChar = key[j % key.length];
+        const keyIndex = alphabet.indexOf(keyChar);
+        let shiftedIndex = (charIndex - keyIndex + 26) % 26; // Decryption: shift left (subtract)
+  
+        let shiftedChar = alphabet[shiftedIndex];
+        if (isUpperCase) {
+          shiftedChar = shiftedChar.toUpperCase();
+        }
+        result += shiftedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+  
+    return result;
+  }
+    
 function encrypt(){
     var origmessage = document.getElementById("messageBox");
     var key = document.getElementById("keyBox");
     var result = document.getElementById("resultBox");
-    result.innerHTML = ojCypher(origmessage.value);
+    
+    var final = origmessage.value;
+
+    //encryption process
+    final = ojCipher(final);
+    final = vigenereEncrypt(final, key.value);
+
+    result.innerHTML = final;
+}
+
+function decrypt(){
+    var origmessage = document.getElementById("messageBox");
+    var key = document.getElementById("keyBox");
+    var result = document.getElementById("resultBox");
+
+    var final = origmessage.value;
+
+    //decryption process
+    final = vigenereDecrypt(final, key.value); 
+    final = ojCipher(final);
+
+    result.innerHTML = final;
 }
